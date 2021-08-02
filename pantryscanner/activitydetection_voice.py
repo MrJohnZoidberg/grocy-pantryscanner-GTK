8a53e8db68df38969f981743ef92f1ee4e575a7c
Dominic Zedler <mrjohnzoidberg@mailbox.org>
2021-08-02 19:40:14 +0200

    Improve VAD

--- pantryscanner/activitydetection_voice.py
+++ pantryscanner/activitydetection_voice.py
@@ -15,32 +15,51 @@ class ActivityDetectionVoice(threading.Thread):
         detection_threshold = self._pantryscanner.get_config_value("sleep", "voice_detection", "detection_threshold")
         self._tun.set_vad_threshold(detection_threshold)
         self._timeout_secs: int = self._pantryscanner.get_config_value("sleep", "sleep_after_secs")
+        self._pause_timer = None
+        self._detection_active = True
         self._terminate = False
         self._new_timer()
 
     def run(self, *args, **kwargs):
-        if self._dev:
-            while not self._terminate:
-                if self._tun.is_voice():
-                    if self._timer and self._timer.is_alive():
-                        self._timer.cancel()
-                    else:
-                        self._pantryscanner.on_activity_detected()
+        if not self._dev:
+            return
 
-                    self._new_timer()
-                    threading.Timer(2, self.run).start()
-                    return
+        if 'do_pause' not in kwargs or not kwargs['do_pause']:
+            self._pause_for_few_secs()
+
+        while not self._terminate:
+            if self._tun.is_voice() and self._detection_active:
+                if self._timer and self._timer.is_alive():
+                    self._timer.cancel()
                 else:
-                    time.sleep(0.05)
-        else:
-            while not self._terminate:
-                time.sleep(0.5)
+                    self._pantryscanner.on_activity_detected()
+
+                self._new_timer()
+                self._pause_for_few_secs()
+            else:
+                time.sleep(0.05)
+
+    def _pause_for_few_secs(self):
+        if self._pause_timer.is_alive():
+            self._pause_timer.cancel()
+        self._detection_active = False
+        self._pause_timer = threading.Timer(3, self._activate_detection)
+        self._pause_timer.start()
+
+    def _activate_detection(self):
+        self._detection_active = True
 
     def _new_timer(self):
-        self._timer = threading.Timer(self._timeout_secs, self._pantryscanner.on_sleep_started)
+        self._timer = threading.Timer(self._timeout_secs, self._start_sleep_and_pause)
         self._timer.start()
+
+    def _start_sleep_and_pause(self):
+        self._pantryscanner.on_sleep_started()
+        self._pause_for_few_secs()
 
     def terminate(self):
         self._terminate = True
-        if self._timer.is_alive():
+        if self._pause_timer and self._pause_timer.is_alive():
+            self._pause_timer.cancel()
+        if self._timer and self._timer.is_alive():
             self._timer.cancel()
--- pantryscanner/barcodescanner.py
+++ pantryscanner/barcodescanner.py
@@ -7,7 +7,6 @@ class BarcodeScanner:
         self._pantryscanner = pantryscanner
         self._gpio_pin_relais: int = self._pantryscanner.get_config_value("scanner", "gpio_pin_relais")
         GPIO.setup(self._gpio_pin_relais, GPIO.OUT)
-        self.on()
 
     def on(self):
         GPIO.output(self._gpio_pin_relais, GPIO.HIGH)
