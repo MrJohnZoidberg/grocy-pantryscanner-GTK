b9a4c71b415e185d6e079dd372ae288f7d463f79
Dominic Zedler <mrjohnzoidberg@mailbox.org>
2021-08-02 18:47:09 +0200

    Set GPIO pin as output and switch scanner on after start

--- pantryscanner/barcodescanner.py
+++ pantryscanner/barcodescanner.py
@@ -6,6 +6,8 @@ class BarcodeScanner:
     def __init__(self, pantryscanner):
         self._pantryscanner = pantryscanner
         self._gpio_pin_relais: int = self._pantryscanner.get_config_value("scanner", "gpio_pin_relais")
+        GPIO.setup(self._gpio_pin_relais, GPIO.OUT)
+        self.on()
 
     def on(self):
         GPIO.output(self._gpio_pin_relais, GPIO.HIGH)
