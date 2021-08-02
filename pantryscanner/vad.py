3dc921d3663a798032925621bbbaef37209f1680
Dominic Zedler <mrjohnzoidberg@mailbox.org>
2021-08-02 16:29:17 +0200

    Add threading.Timer to VAD

--- pantryscanner/vad.py
+++ pantryscanner/vad.py
@@ -10,24 +10,31 @@ class VAD(threading.Thread):
     def __init__(self, pantryscanner):
         super().__init__()
         self._pantryscanner = pantryscanner
-        self._terminate = False
         self._dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
+        self._tun = tuning.Tuning(self._dev)
+        self._terminate = False
+        self._timer = threading.Timer(10, self.on_timer_finished)
 
     def run(self, *args, **kwargs):
         if self._dev:
-            tun = tuning.Tuning(self._dev)
             while not self._terminate:
-                try:
-                    if tun.is_voice():
-                        self._pantryscanner.screen_dimmed()
-                        time.sleep(5)
-                        self._pantryscanner.screen_bright()
+                if self._tun.is_voice():
+                    self.on_voice_detected()
+                    self._timer.start()
+                    return
+                else:
                     time.sleep(0.05)
-                except KeyboardInterrupt:
-                    break
         else:
             while not self._terminate:
                 time.sleep(0.5)
 
+    def on_voice_detected(self):
+        self._pantryscanner.screen_dimmed()
+
+    def on_timer_finished(self):
+        self._pantryscanner.screen_bright()
+        self.run()
+
     def terminate(self):
         self._terminate = True
+        self._timer.cancel()
