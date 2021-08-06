8daca3c7ef1193834033c7d0ee44d372adeb3368
Dominic Zedler <mrjohnzoidberg@mailbox.org>
2021-08-06 17:23:31 +0200

    Mic LEDS off on start

--- pantryscanner/speechrecognition.py
+++ pantryscanner/speechrecognition.py
@@ -11,7 +11,8 @@ class SpeechRecognition:
         self._pantryscanner = pantryscanner
         self._bb_api_url = self._pantryscanner.get_config_value('barcodebuddy', 'bb_server_url') + "api/"
         self._bb_api_key = self._pantryscanner.get_config_value('barcodebuddy', 'bb_api_key')
-        pixel_ring.change_pattern('echo')
+        pixel_ring.off()
+        pixel_ring.set_vad_led(0)
 
     def start_recording(self):
         self._recording_thread = threading.Thread(target=self._record_speech)
@@ -21,7 +22,7 @@ class SpeechRecognition:
         # obtain audio from the microphone
         with sr.Microphone() as source:
             print("Say something!")
-            pixel_ring.wakeup()
+            pixel_ring.spin()
             audio = self._r.listen(source)
         pixel_ring.off()
         self._result(audio)
